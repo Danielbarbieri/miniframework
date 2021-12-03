088fab7d4995f075399aeb93b8760dd14d33f79f
Daniel Barbieri <danielbarbierianalista@gmail.com>
2021-12-03 15:29:06 -0300

    fim controllers

--- Public/index.php
+++ Public/index.php
@@ -1,10 +1,17 @@
 <?php
-    require_once"../vendor/autoload.php";
+require_once "../vendor/autoload.php";
 
-    $route = new \App\Route;
-    echo 'Rotas funcionando';
-    echo '<hr>';
-    print_r($route->getUrl());
+$route = new \App\Route;
+echo 'Rotas funcionando as 15h';
+echo '<hr>';
+print_r($route->getUrl());
+echo '<hr>';
+echo '<pre>';
+print_r($route->getRoutes());
+echo '</pre>';
+?>
 
-    
+<?php
+
+
 ?>
